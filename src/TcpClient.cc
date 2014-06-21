6c28a540ff571e2d4ae7a43b76254b665a426ea4
bingyu.song <songbingyu918@gmail.com>
2014-06-21 17:04:23 +0800

    add tcp client

---
+++ src/TcpClient.cc
@@ -0,0 +1,25 @@
+/*
+ *  CopyRight  2014 , bingyu.song   All Right Reserved
+    I believe  Spring brother
+ */
+
+#include"TcpClient.h"
+
+
+TcpClient::TcpClient()
+{
+
+
+}
+
+TcpClient::~TcpClient()
+{
+
+
+}
+
+
+
+
+
+
---
+++ src/TcpClient.h
@@ -0,0 +1,22 @@
+/*
+ *  CopyRight  2014 , bingyu.song   All Right Reserved
+    I believe  Spring brother
+ */
+
+#ifndef     _TCP_CLIENT_H_
+#define     _TCP_SERVER_H_
+
+class    TcpClient
+{
+public:
+    TcpClient();
+    ~TcpClient();
+private:
+
+};
+
+#define     _TCP_SERVER_H_
+
+
+
+

66ba52547c3176ff8c0836b103b7a5696feffc89
bingyu.song <songbingyu918@gmail.com>
2014-06-22 10:53:26 +0800

    epoll

---
+++ src/EventEpoll.cc
@@ -0,0 +1,20 @@
+/*
+ *  CopyRight  2014 , bingyu.song   All Right Reserved
+    I believe  Spring brother
+ */
+
+
+#include< EventEpoll.h >
+
+EventEpoll::EventEpoll()
+{
+
+}
+
+EventEpoll::~EventEpoll()
+{
+
+}
+
+
+
---
+++ src/EventEpoll.h
@@ -0,0 +1,26 @@
+/*
+ *  CopyRight  2014 , bingyu.song   All Right Reserved
+    I believe  Spring brother
+ */
+
+#ifndef     _EVENTEPOLL_H_
+#define     _EVENTEPOLL_H_
+
+// epoll wraper
+class  EventEpoll
+{
+ public :
+    EventEpoll();
+    ~EventEpoll();
+
+private:
+
+
+};
+
+
+#endif //_EVENTEPOLL_H_
+
+
+
+

f499ed59bad34bd22cd6f2ee4908274c04b65d5f
bingyu.song <songbingyu918@gmail.com>
2014-06-23 10:52:23 +0800

    add eventloop

---
+++ src/EventLoop.cc
@@ -0,0 +1,27 @@
+/*
+ *  CopyRight  2014 , bingyu.song   All Right Reserved
+    I believe  Spring brother
+ */
+
+
+#include "EventLoop.h"
+
+EventLoop::EventLoop()
+{
+
+
+}
+
+EventLoop::~EventLoop()
+{
+
+
+}
+
+int  EventLoop::run()
+{
+
+
+}
+
+
---
+++ src/EventLoop.h
@@ -0,0 +1,30 @@
+/*
+ *  CopyRight  2014 , bingyu.song   All Right Reserved
+    I believe  Spring brother
+ */
+
+#ifndef   _EVENTLOOP_H_
+#define   _EVENTLOOP_H_
+
+
+#include "EventEpoll.h"
+
+
+class  EventLoop
+{
+public :
+     EventLoop();
+     ~EventLoop();
+public:
+     int run();
+private:
+
+     EventEpoll eventEpoll_;
+};
+
+
+#endif // _EVENTLOOP_H_
+
+
+
+

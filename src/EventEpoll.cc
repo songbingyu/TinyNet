0f58ad8c3bc66a92d5c8d4a460dc1780ee2bd1a3
bingyu.song <songbingyu918@gmail.com>
2014-06-24 01:24:31 +0800

    improve epoll

--- src/EventEpoll.cc
+++ src/EventEpoll.cc
@@ -9,6 +9,7 @@
 #include <sys/epoll.h>
 #include <unistd.h>
 #include "Log.h"
+#include "IConnection.h"
 #include "EventEpoll.h"
 
 
@@ -39,39 +40,86 @@ int EventEpoll::epollCreate( )
 }
 
 
-int EventEpoll::addEvent( )
+int EventEpoll::addEvent( IConnection* conn  )
 {
+    int fd = conn->getSockFd();
+
+    if( conn->getEvents() == 0  )
+    {
+        LOG_ERROR( " socket %d not set events", fd );
+        return 1;
+    }
+
+
+    ConnectionMap::iterator  it = connectionMap.find( fd );
+    if ( it != connectionMap.end() )
+    {
+        connectionMap[ fd ] = conn;
+    }
+
+    epollCtl( EPOLL_CTL_ADD, conn );
+
     return 1;
 }
 
-int EventEpoll::delEvent()
+int EventEpoll::delEvent( IConnection* conn )
 {
+
+    int fd = conn->getSockFd();
+    ConnectionMap::iterator  it = connectionMap.find( fd );
+    if ( it == connectionMap.end() )
+    {
+        LOG_ERROR( " not find socket %d in connectionMap ", fd );
+        return 1;
+    }
+
+    if( it->second == conn )
+    {
+        LOG_ERROR( " I don't know what happen ?  ", fd );
+        return 1;
+    }
+    connectionMap.erase( it );
+
+    epollCtl( EPOLL_CTL_DEL, conn );
 
     return 1;
 
 }
 
-int EventEpoll::modEvent()
+
+
+int EventEpoll::epollCtl( int oper, IConnection* conn )
 {
+    struct epoll_event event;
+    bzero( &event, sizeof(event) );
+    event.events   = conn->getEvents();
+    event.data.ptr = conn;
+    int sockfd     = conn->getSockFd();
+
+    int ret = epoll_ctl( epollfd_, oper, sockfd, &event );
+    if( ret < 0 )
+    {
+        LOG_ERROR(" epool_ctl faila...:%d ", oper );
+    }
 
     return 1;
 }
 
-
-int EventEpoll::waitEvent( int timeout, ConnectionVec*  activeConns )
+int EventEpoll::waitEvent( int timeout, std::vector<IConnection*>*  activeConns )
 {
 #ifdef _DEBUG_
     assert( epollfd_ != NULL );
 #endif
-    int numEvents =  epoll_wait( epollfd_, &*events.begin(), (int)events.size(), timeout );
+    int numEvents =  epoll_wait( epollfd_, &*events_.begin(), (int)events_.size(), timeout );
     if( numEvents > 0 )
     {
 
-            assert( numEvents < events.size() );
+            assert( numEvents < (int)events_.size() );
             for( int i=0; i < numEvents; ++i )
             {
-               IConnections* conn = (IConnections*)eventd_[i].data.ptr;
-
+                IConnection* conn = (IConnection*)events_[i].data.ptr;
+                conn->setReadEvents( events_[i].events );
+                activeConns->push_back( conn );
             }
     }
     else if ( numEvents == 0  )
@@ -81,7 +129,7 @@ int EventEpoll::waitEvent( int timeout, ConnectionVec*  activeConns )
     else
     {
 
-        LOG_INFO(" no events:%d "i, errno );
+        LOG_INFO(" no events:%d ", errno );
     }
     return 1;
 }
--- src/EventEpoll.h
+++ src/EventEpoll.h
@@ -6,38 +6,36 @@
 #ifndef     _EVENTEPOLL_H_
 #define     _EVENTEPOLL_H_
 
-#include<map>
-#include<vector>
-
+#include <map>
+#include <vector>
 
 struct      epoll_event;
+class       IConnection;
 
 
 // epoll wraper
-class  EventEpoll
-{
- public :
+class  EventEpoll {
+public :
     EventEpoll();
     ~EventEpoll();
- public:
+public:
 
     int epollCreate();
 
-    int addEvent();
+    int addEvent( IConnection* conn );
 
-    int delEvent();
+    int delEvent( IConnection* conn );
 
-    int modEvent();
+    int waitEvent( int timeout, std::vector<IConnection*>*  activeConns  );
 
-    int waitEvent( int timeout, ConnectionVec*  activeConns  );
-
- public:
- private:
+private:
+    int  epollCtl( int oper, IConnection* conn );
+private:
 
     int epollfd_;
     typedef  std::vector< struct epoll_event >  EventVec;
     EventVec            events_;
-    typedef  std::map< int, IConnections* > ConnectionMap;
+    typedef  std::map< int, IConnection* >     ConnectionMap;
     ConnectionMap       connectionMap;
 
 };

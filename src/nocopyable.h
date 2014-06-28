e4952a50e9a50b1e4d2821031102a0d7517afee0
bingyu.song <songbingyu918@gmail.com>
2014-06-28 13:56:06 +0800

    add nocopyable util

---
+++ src/nocopyable.h
@@ -0,0 +1,26 @@
+/*
+ *  CopyRight  2014 , bingyu.song   All Right Reserved
+    I believe  Spring brother
+ */
+
+#ifndef _NOCOPYABLE_H_
+#define _NOCOPYABLE_H_
+
+
+//from boost::nocopyable
+class nocopyable
+{
+protected:
+    nocopyable()  { }
+    ~nocopyable() { }
+private:
+    nocopyable( nocopyable& );
+    nocopyable& operator=( nocopyable& );
+}
+
+
+#endif // _NOCOPYABLE_H_
+
+
+
+

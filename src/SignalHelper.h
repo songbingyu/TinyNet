85844c88aab9c084144f4a4724e73fa74db8dc2a
bingyu.song <songbingyu918@gmail.com>
2014-10-15 17:33:37 +0800

    add signal proc

---
+++ src/SignalHelper.h
@@ -0,0 +1,22 @@
+/*
+ *  CopyRight  2014 , bingyu.song   All Right Reserved
+    I believe  Spring brother
+ */
+#ifndef _SIGNAL_HELPER_H_
+#define _SIGNAL_HELPER_H_
+
+class SignalHelper
+{
+public:
+    SignalHelper();
+    ~SignalHelper();
+public:
+
+private:
+
+
+};
+
+
+#endif //_SIGNAL_HELPER_H_
+

0d5ffbd496e560989ec5180bd12f30c45795086c
bingyu.song <songbingyu918@gmail.com>
2014-11-05 10:52:00 +0800

    test Timer

--- exam/server_test.cc
+++ exam/server_test.cc
@@ -2,11 +2,44 @@
 
 #include"TcpServer.h"
 
+#include "TcpClient.h"
+
+#include "Event.h"
+#include "EventLoop.h"
+
+void onTimer( EventLoop* loop, IEvent* ev, int revents )
+{
+    LOG_INFO("onTimer");
+}
+
+
 int main( )
 {
-    TcpServer  server;
+    //TcpServer  server;
+    //
 
-    server.run();
+    //TcpClient client("182.92.218.149", 8001 );
+
+   // client.init();
+
+    //client.connect();
+
+    //client.run();
+
+    //server.run();
+    //
+    //
+
+
+    EventLoop* loop = new EventLoop();
+
+    EventTimer timer( loop, onTimer, 1,1 );
+
+    timer.start();
+
+    loop->run( EVRUN_ALWAYES );
+
+
 
 }
 
